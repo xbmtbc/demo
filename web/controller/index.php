c1b51aee21dbf91baeb80204bdda03626380d4d3
xbmtbc <960524501@qq.com>
2018-06-14 12:29:21 +0800

    测试

---
+++ web/controller/index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/6/14
+ * Time: 12:27
+ */
+namespace web\controller;
+class web
+{
+    public function show(){
+        echo "web";
+    }
+}
